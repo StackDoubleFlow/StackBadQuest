3ac27c4a73eaed00e8950ac34647f73d1aa7c683
StackDoubleFlow <ojaslandge@gmail.com>
2021-08-16 08:04:05 -0500

    stack bad

--- copy.sh
+++ copy.sh
@@ -8,8 +8,8 @@ stack_bad src/main.sb
 $LD -shared src/main.o extern/libmodloader.so extern/libbeatsaber-hook_2_2_5.so -o libstack_bad.so
 $STRIP libstack_bad.so
 
-adb push libstack_bad.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libstack_bad
-# adb shell am force-stop com.beatgames.beatsaber
-# adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+adb push libstack_bad.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libstack_bad.so
+adb shell am force-stop com.beatgames.beatsaber
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
 
-# adb logcat -c && adb logcat > test.log
+adb logcat -c && adb logcat > test.log
